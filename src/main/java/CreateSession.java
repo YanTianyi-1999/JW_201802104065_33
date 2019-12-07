8f791f80c6e1ca4effe4b07234a7fb2204896948
YanTianyi-1999 <1337123146@qq.com>
2019-12-07 11:50:04 +0800

    Initial commit

---
+++ src/main/java/CreateSession.java
@@ -0,0 +1,18 @@
+//201802104065闫天意
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/createSession")
+public class CreateSession extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        session.setMaxInactiveInterval(20);
+        resp.getWriter().println("Session will last 20 seconds.");
+    }
+}
